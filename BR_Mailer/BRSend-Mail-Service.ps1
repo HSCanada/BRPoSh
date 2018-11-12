17cc3c2d091d49b35c3754f88e8bf2c73571fd06
Crowley <Trevor.Crowley@henryschein.ca>
2018-11-12 17:22:27 -0500

    cleanup service script

--- BR_Mailer/BRSend-Mail-Service.ps1
+++ BR_Mailer/BRSend-Mail-Service.ps1
@@ -1,42 +1,26 @@
-﻿
+﻿# Send Service Email
+# 12 Nov 18
 
 $smtp=$env:BR_Mailing_Server
 
-$path=$env:br_mailing_path  #(get-location) 
+$path=$env:br_mailing_path
 
+$path=$path + 'service\'
+$body = ''
 
-$Logfile = "$path" + "log\mailing.log" 
-
-
-
-Function LogWrite
-{
-   Param ([heltring]$logstring)
-
-   Add-content $Logfile -value $logstring
-}
-
-# Path & file name for msg body
-#$body=get-content ($path +"service\Email.Msg.Body\Service File Link.htm") | out-string
-$body=get-content ($path +"service\Email.Msg.Body\Service - Mail Body.htm") | out-string
-
-
-Clear-Host
-if (!(test-path HKLM:\SYSTEM\CurrentControlSet\Services\Eventlog\Application\BRC))`
-    {new-eventlog -Logname Application -source BRC `
-    -ErrorAction SilentlyContinue}
+#debug
+echo $smtp
+echo $path
 
 # Path & file name for mailing list
-$list=import-csv ($path + "service\"+"Service_Mailing_List.csv")
+$list=import-csv ($path + 'Service_Mailing_List.csv')
 
+# Path & file name for msg body
+$body=get-content ($path +'Email.Msg.Body\Service - Mail Body3.htm') | out-string
 
 
 $startTime = Get-date
 $startLog = 'Service-' +$startTime
-
-
-
-write-eventlog -logname Application -message $startlog -source BRC -ENTRYTYPE information -EventId 2 -category 1
 
 foreach ($i in $list)
 {	
@@ -96,14 +80,11 @@ foreach ($i in $list)
 		
 	       send-mailmessage -smtpserver $smtp -to $emailarray -from "businessreporting.canada@henryschein.ca" -subject $i.subject -body $body -bodyashtml @params 
 
-         
-            write-eventlog -logname Application -message ( 'Service -' +$startTime+ '  to  '  + $i.email + "    " + $attach ) -source BRC -ENTRYTYPE information -EventId 2 -category 1
-           
+          
          }
         catch
          {
              echo "sending message failed"
-             write-eventlog -logname Application -message ('Service -' +$startTime+ '  to  '  + $i.email + "   - Fail to send") -source BRC -ENTRYTYPe FailureAudit -EventId 2 -category 1
 
         }
  
@@ -117,13 +98,11 @@ foreach ($i in $list)
                 #Write-Host $body
                 send-mailmessage -smtpserver $smtp -to $emailarray  -from "businessreporting.canada@henryschein.ca" -subject $i.subject -body $body -bodyashtml 
                 
-                write-eventlog -logname Application -message ( 'Service -' +$startTime+ '  to  '  + $i.email ) -source BRC -ENTRYTYPE information -EventId 2 -category 1
-               
+              
             }
             catch{
                     echo "sending message failed"
                    
-                    write-eventlog -logname Application -message ('Service -' +$startTime+ '  to  '  + $i.email + "   - Fail to send") -source BRC -ENTRYTYPe FailureAudit -EventId 2 -category 1
             }
       }
       else
@@ -132,17 +111,14 @@ foreach ($i in $list)
              try{
                 send-mailmessage -smtpserver $smtp -to $emailarray  -from "businessreporting.canada@henryschein.ca" -subject $i.subject -body $body -bodyashtml 
                 
-                write-eventlog -logname Application -message ( 'Service -' +$startTime+ '  to  '  + $i.email ) -source BRC -ENTRYTYPE information -EventId 2 -category 1
                
             }
             catch{
                     echo "sending message failed"
                    
-                    write-eventlog -logname Application -message ('Service -' +$startTime+ '  to  '  + $i.email + "   - Fail to send") -source BRC -ENTRYTYPe FailureAudit -EventId 2 -category 1
             }
       }
     
 
 }
-
 
