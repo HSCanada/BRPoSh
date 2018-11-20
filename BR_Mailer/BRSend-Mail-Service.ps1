b30fbb8a5c3a9b40387a5e7e6740b17a7a87184d
Crowley <Trevor.Crowley@henryschein.ca>
2018-11-20 10:42:45 -0500

    cleanup code

--- BR_Mailer/BRSend-Mail-Service.ps1
+++ BR_Mailer/BRSend-Mail-Service.ps1
@@ -2,124 +2,73 @@
 # created by jli
 # 16 Nov 18, tmc, mail from updated
 
+# todo:  
+#  group email?
+#  make parameter driven (body, emails, attach) -- flowchat
+
+# Set email server 
 $smtp=$env:BR_Mailing_Server
 
+# Set email parameters location
 $path=$env:br_mailing_path
-
 $path=$path + 'service\'
-$body = ''
 
-#debug
-echo $smtp
-echo $path
-
-# Path & file name for mailing list
+# Load mailing list
 $list=import-csv ($path + 'Service_Mailing_List.csv')
 
-# Path & file name for msg body
+# Load message body
 $body=get-content ($path +'Email.Msg.Body\Service - Mail Body3.htm') | out-string
 
-
+# performace log start
 $startTime = Get-date
 $startLog = 'Service-' +$startTime
 
+# mail for each user
 foreach ($i in $list)
 {	
-	if($i.flag -eq 1 -and $i.count -gt 0) {
-		
-		 
-        if($i.attach1 ){
-	        $attachments=@()
-            $attach=@()
-   
-            $attachments += $i.attach1 
+    # build up attachments array (max 3)
+    $attachments=@()
 
-            $attach +=$i.attach1
-
-           
-          }
-        if($i.attach2){
-            $attachments=@()
-            $attach=@()
-
-            $attachments += $i.attach1 
-            $attach +=$i.attach1
- 
-            $attachments += $i.attach2 
-            $attach +=$i.attach2
-            
-
-        }
-		
-        if($i.attach3){
-
-		
-	    $attachments=@()
-            $attach=@()
-
-
-            $attachments += $i.attach1 
-            $attach +=$i.attach1
-	
-            $attachments += $i.attach2 
-            $attach +=$i.attach2
-	
-            $attachments += $i.attach3 
-            $attach +=$i.attach3
-           
-         
-        }                                           
-        echo $attachments	
-
-	    $params = @{}
-	
-	    $params['Attachments'] = $attachments
-	
-       	    $emailarray  = $I.EMAIL -split ','
-
-        try{ 
-		
-	       send-mailmessage -smtpserver $smtp -to $emailarray -from "David.Pinto@henryschein.ca" -subject $i.subject -body $body -bodyashtml @params 
-
+    if($i.attach1 ) {
+        $attachments += $i.attach1 
+    }
           
-         }
-        catch
-         {
-             echo "sending message failed"
-
-        }
- 
+    if($i.attach2) {
+        $attachments += $i.attach2 
     }
 
-    elseif($i.flag -eq 1) {
-    
-        $emailarray  = $I.EMAIL -split ','
-            try{
+    if($i.attach3) {
+        $attachments += $i.attach3 
+    }
 
-                #Write-Host $body
-                send-mailmessage -smtpserver $smtp -to $emailarray  -from "David.Pinto.canada@henryschein.ca" -subject $i.subject -body $body -bodyashtml 
-                
-              
-            }
-            catch{
-                    echo "sending message failed"
-                   
-            }
-      }
-      else
-       
-      {
-             try{
-                send-mailmessage -smtpserver $smtp -to $emailarray  -from "David.Pinto.canada@henryschein.ca" -subject $i.subject -body $body -bodyashtml 
-                
-               
-            }
-            catch{
-                    echo "sending message failed"
-                   
-            }
-      }
-    
+    # convert attachement array to named list           
 
-}
+	$params = @{}
+	$params['Attachments'] = $attachments
+
+    # build mail send (can be list)
+    $emailarray  = $I.EMAIL -split ','
+
+
+    # Send email
+    try { 
+        Write-Host $emailarray 
+#        send-mailmessage -smtpserver $smtp -to $emailarray -from "David.Pinto@henryschein.ca" -subject $i.subject -body $body -bodyashtml @params 
+    } 
+    catch {
+        echo "sending message failed"
+    }
+
+} # for
+
+# testing
+#$mailto = 'trevor.crowley@henryschein.ca'
+ $mailto = 'trevor.crowley@henryschein.ca', 'jennifer.li@henryschein.ca'
+
+send-mailmessage -smtpserver $smtp -to $mailto -from "David.Pinto@henryschein.ca" -subject $i.subject -body $body -bodyashtml @params 
+
+ 
+   
+
+
 
