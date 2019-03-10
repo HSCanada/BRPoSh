6905bed1d5493bbc5b678453042f0453049a8894
trevor <trevor.crowley@henryschein.ca>
2019-03-10 10:37:11 -0400

    reorg

--- BR-Bitrix/Get-BXGroup.ps1
+++
@@ -1,8 +0,0 @@
-﻿Import-Module -Force -Name .\InvokeSQL.psm1 
-
-$cmd = "SELECT * FROM nes.bx_group_load"
-
-$DataRows = Invoke-MSSQL -Server $env:bx_server -database $env:bx_database -SQLCommand $cmd -ConvertFromDataRow:$false
-
-write-output $DataRows 
-
