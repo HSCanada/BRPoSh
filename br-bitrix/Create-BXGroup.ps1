975eee22837c8b8cbf55b9257693cb2e6364c81b
trevor <trevor.crowley@henryschein.ca>
2019-03-10 10:35:59 -0400

    reorg

---
+++ br-bitrix/Create-BXGroup.ps1
@@ -0,0 +1,65 @@
+
+param(
+    [parameter(
+        Mandatory         = $true,
+        ValueFromPipeline = $true)]
+    $pipelineInput
+)
+<#
+$params_create = @{
+    bx_shipto = 3795447
+    bx_user_id_fsc = 6
+    bx_user_id_ess = 4
+    bx_user_id_branch = 33
+    NAME = "Elsaraj Dentistry Prof Corp - 3795447 - TBD"
+    DESCRIPTION = "1596 Walkley Rd |  | Ottawa | ON | (613) 738-7211  | jonathan hollink | bev robertson"
+    VISIBLE = "Y"
+    OPENED = "N"
+    KEYWORDS = "ottawa"
+    INITIATE_PERMS = "K"
+    PROJECT = "Y"
+    PROJECT_DATE_START  = "31/03/2019 12:00:00 AM"
+    PROJECT_DATE_FINISH = "31/12/2040 12:00:00 AM"
+}
+#>
+
+$url_base = $env:bx_webhook_url
+
+$url_create = $url_base + "sonet_group.create/"
+$url_invite = $url_base + "sonet_group.user.invite/"
+$url_setowner = $url_base + "sonet_group.usonet_group.setowner/"
+
+# create group
+$res_create = Invoke-RestMethod -Method 'Post' -Uri $url_create -Body $pipelineInput
+$group_id = $res_create.result
+
+# assign users to group
+$params_invite = @{
+    GROUP_ID = $group_id
+    USER_ID = $pipelineInput.bx_user_id_fsc
+    MESSAGE = "Invitation"
+}
+$res_invite = Invoke-RestMethod -Method 'Post' -Uri $url_invite -Body $pipelineInput
+
+$params_invite = @{
+    GROUP_ID = $group_id
+    USER_ID = $pipelineInput.bx_user_id_ess
+    MESSAGE = "Invitation"
+}
+$res_invite = Invoke-RestMethod -Method 'Post' -Uri $url_invite -Body $pipelineInput
+
+$params_invite = @{
+    GROUP_ID = $group_id
+    USER_ID = $piplineInput.bx_user_id_branch
+}
+$res_invite = Invoke-RestMethod -Method 'Post' -Uri $url_setownder -Body $pipelineInput
+
+# return new group for post processing
+$output += @{
+    SHIPTO = $pipelineInput.bx_shipto
+    BX_GROUP_ID = $group_id
+    BX_SET_DATE = $pipelineInput.PROJECT_DATE_START
+}
+
+write-output $output
+
---
+++ br-bitrix/Get-BXGroup.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module -Force -Name .\InvokeSQL.psm1 
+
+$cmd = "SELECT * FROM nes.bx_group_load"
+
+$DataRows = Invoke-MSSQL -Server $env:bx_server -database $env:bx_database -SQLCommand $cmd -ConvertFromDataRow:$false
+
+write-output $DataRows 
+
