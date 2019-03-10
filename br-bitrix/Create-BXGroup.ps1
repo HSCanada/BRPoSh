ecd52377434015e5c3aadd9a2d64c20e41ee431d
trevor <trevor.crowley@henryschein.ca>
2019-03-10 17:54:45 -0400

    pre pipeline

--- br-bitrix/Create-BXGroup.ps1
+++ br-bitrix/Create-BXGroup.ps1
@@ -5,23 +5,6 @@ param(
         ValueFromPipeline = $true)]
     $pipelineInput
 )
-<#
-$params_create = @{
-    bx_shipto = 3795447
-    bx_user_id_fsc = 6
-    bx_user_id_ess = 4
-    bx_user_id_branch = 33
-    NAME = "Elsaraj Dentistry Prof Corp - 3795447 - TBD"
-    DESCRIPTION = "1596 Walkley Rd |  | Ottawa | ON | (613) 738-7211  | jonathan hollink | bev robertson"
-    VISIBLE = "Y"
-    OPENED = "N"
-    KEYWORDS = "ottawa"
-    INITIATE_PERMS = "K"
-    PROJECT = "Y"
-    PROJECT_DATE_START  = "31/03/2019 12:00:00 AM"
-    PROJECT_DATE_FINISH = "31/12/2040 12:00:00 AM"
-}
-#>
 
 $url_base = $env:bx_webhook_url
 
