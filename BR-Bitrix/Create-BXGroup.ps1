c99ac84d1b11afc175885061f32335016d264600
trevor-crowley <trevor_crowley@hotmail.com>
2019-03-10 10:26:28 -0400

    add set ownder logic

--- BR-Bitrix/Create-BXGroup.ps1
+++ BR-Bitrix/Create-BXGroup.ps1
@@ -27,6 +27,7 @@ $url_base = $env:bx_webhook_url
 
 $url_create = $url_base + "sonet_group.create/"
 $url_invite = $url_base + "sonet_group.user.invite/"
+$url_setowner = $url_base + "sonet_group.usonet_group.setowner/"
 
 # create group
 $res_create = Invoke-RestMethod -Method 'Post' -Uri $url_create -Body $pipelineInput
@@ -50,12 +51,11 @@ $res_invite = Invoke-RestMethod -Method 'Post' -Uri $url_invite -Body $pipelineI
 $params_invite = @{
     GROUP_ID = $group_id
     USER_ID = $piplineInput.bx_user_id_branch
-    MESSAGE = "Invitation"
 }
-$res_invite = Invoke-RestMethod -Method 'Post' -Uri $url_invite -Body $pipelineInput
+$res_invite = Invoke-RestMethod -Method 'Post' -Uri $url_setownder -Body $pipelineInput
 
 # return new group for post processing
-$output = @{
+$output += @{
     SHIPTO = $pipelineInput.bx_shipto
     BX_GROUP_ID = $group_id
     BX_SET_DATE = $pipelineInput.PROJECT_DATE_START
