d68154aff68b9ea01b54ffc39cb2224f47467009
trevor <trevor.crowley@henryschein.ca>
2019-03-10 20:05:16 -0400

    date format

--- br-bitrix/Set-BXGroup.ps1
+++ br-bitrix/Set-BXGroup.ps1
@@ -14,7 +14,7 @@
 	Process {
                 ForEach ($rec in $pipelineInput) {
  
-			$cmd = "EXEC [nes].[bx_group_update_proc] @Shipto = {0}, @GroupId = {1}, @SetDate = {2}" -f $rec.BX_SHIPTO, $rec.BX_GROUP_ID, $rec.BX_SET_DATE 
+			$cmd = "EXEC [nes].[bx_group_update_proc] @Shipto = {0}, @GroupId = {1}, @SetDate ='{2:yyyyMMdd}'" -f $rec.BX_SHIPTO, $rec.BX_GROUP_ID, $rec.BX_SET_DATE 
 
 	#		$cmd
 
