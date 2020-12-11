b037f8e75ff8911b078d5e2efb2e6d5586c8e2de
Crowley <Trevor.Crowley@henryschein.ca>
2020-12-11 10:11:32 -0500

    maint

--- Bitrix_Maint/Get-BXGroupPermission.ps1
+++ Bitrix_Maint/Get-BXGroupPermission.ps1
@@ -16,12 +16,12 @@ $cmd = "SELECT * FROM nes.bx_group_permission_load ORDER BY GROUP_ID, USER_ID"
 
 ## update USER_ID and branch
 
-#$cmd = @"
-#SELECT        c.ShipTo AS bx_shipto, c.bx_group_id AS GROUP_ID, 374 AS USER_ID 
-#FROM            BRS_Customer AS c INNER JOIN
-#                        BRS_FSC_Rollup AS f ON c.TerritoryCd = f.TerritoryCd
-#WHERE        (c.bx_group_id <> '') AND (f.Branch LIKE 'C%') AND (c.bx_install_date >= '20201001')
-#"@
+# $cmd = @"
+# SELECT        c.ShipTo AS bx_shipto, c.bx_group_id AS GROUP_ID, 407 AS USER_ID 
+# FROM            BRS_Customer AS c INNER JOIN
+#                         BRS_FSC_Rollup AS f ON c.TerritoryCd = f.TerritoryCd
+# WHERE        (c.bx_group_id <> '') AND (f.Branch LIKE 'MNTRL%') AND (c.bx_install_date >= '20201201')
+# "@
 
 $DataRows = Invoke-MSSQL -Server $bx_server -database $bx_database -SQLCommand $cmd -ConvertFromDataRow:$false
 
