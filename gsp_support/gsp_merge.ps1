582b40a74d1a1794e416b5518357c79087ccf685
trevor-crowley <trevor_crowley@hotmail.com>
2021-03-15 22:50:40 -0400

    working script completed

---
+++ gsp_support/gsp_merge.ps1
@@ -0,0 +1,49 @@
+<#
+.Synopsis
+gsp_merge - merges B and S GSP files to a common file
+.DESCRIPTION
+Long description
+.INPUTS
+*.csv files from GSP
+.OUTPUTS
+output=./load
+archive=./zArchive
+.NOTES
+date on S and B files are different yyyymmdd vs yyyy-mm-dd
+will handle conversion downstream from file
+#>
+
+# init params
+$IN_FILE='CN-SCHN*.csv'
+$OUT_FILE='.\load\CN-SCHN_LOAD.csv' 
+$ARCHIVE_PATH='.\zArchive'
+$i=1
+
+try
+{
+	# clear output file
+	Remove-Item -Path $OUT_FILE
+
+	# build output file
+	# add filename and line number to merged source files
+	#
+	Get-ChildItem -Filter $IN_FILE -PipelineVariable fileObj | 
+	Import-Csv | 
+	ForEach-Object {
+	Add-Member -InputObject $PSItem -MemberType NoteProperty -Name 'FILE_LINE' -Value ($i++) -PassThru; } | 
+	Select-Object @{n='FILE_NAME'; e={$fileObj.Name }},
+		FILE_LINE, ORDERNO, REFERENCE, ITEMNO, ITEMDESC,
+		UPC,QTY,PRICE,FREEGDS,DATE,ACCOUNT,
+		COMPANY,FIRSTLAST,ADDRESS1,ADDRESS2,ADDRESS3,
+		CITY,ST,POSTALCODE,PHONE,COUNTRY,PROGRAMCODE,PROMOCODE | 
+	Export-Csv $OUT_FILE -NoTypeInformation -Append
+	
+	# archive inputs, if no error
+	Move-Item -Path $IN_FILE -DESTINATION $ARCHIVE_PATH
+}
+catch
+{
+	write-output 'Error:  unable to prepare load file!!'
+}
+	
+
--- gsp_support/runmerge.ps1
+++
@@ -1,1 +0,0 @@
-Get-ChildItem -Filter *.csv | Import-Csv | Export-Csv .\merged\merged.csv -NoTypeInformation -Append
